d718ccd3a71f427b91c110e4677981f4fc3d3dff
Alex <alexweldetsadik@gmail.com>
2016-09-25 19:23:23 +0000

    implemented CRUD operations

--- test/test-server.js
+++ test/test-server.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('./server');
+var server = require('../server');
 
 var should = chai.should();
 var app = server.app;
