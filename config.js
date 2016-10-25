2f7cff65574fd623f35f280996c3314f421392af
Alex <alexweldetsadik@gmail.com>
2016-10-25 21:27:37 +0000

    setup db

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://shoppinglist:FullStack1@ds011291.mlab.com:11291/mongo':
-                            'mongodb://shoppinglist:FullStack1@ds011291.mlab.com:11291/mongo');
+                       global.DATABASE_URL || 'mongodb://shoppinglist:FullStack1@ds011291.mlab.com:11291/mongo';
 exports.PORT = process.env.PORT || 8080;
